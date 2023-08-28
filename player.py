49b4c0013d274096b09557914c4421b1d7b3228f
Chamath Jayarathne <chamathsocial@gmail.com>
2023-08-28 13:22:19 -0400

    feat: Create Player

---
+++ player.py
@@ -0,0 +1,17 @@
+from turtle import Turtle
+
+STARTING_POSITION = (0, -280)
+MOVE_DISTANCE = 10
+FINISH_LINE_Y = 280
+
+
+class Player(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.shape("turtle")
+        self.penup()
+        self.setheading(90)
+        self.sety(-280)
+
+    def move(self):
+        self.goto(0, self.ycor() + MOVE_DISTANCE)
